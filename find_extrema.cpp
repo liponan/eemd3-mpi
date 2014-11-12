21c240c8c6174b4294142559ea790c41e9e81e4b
Po-Nan Li <lishantreasure@gmail.com>
2014-11-12 17:55:10 +0800

    remove commented lines

--- find_extrema.cpp
+++ find_extrema.cpp
@@ -6,23 +6,9 @@
 
 void find_extrema(int *Pmax, double *Vmax, int *Pmin, double *Vmin, int *Lmax, int *Lmin,
 				double *Y, int sz) {
-	//double *Pmax, *Pmin;
-	//double *Y, *Vmax, *Vmin;
-	//int m, n, *Lmax, *Lmin;
-	//fstream fout("spline_log.txt", ios::out | ios::app);
-	/* parse input arguments */
-	//Y  = mxGetPr(prhs[0]);
-	//m = mxGetM(prhs[0]);
-	//n = mxGetN(prhs[0]);
-	//int *Lmax = *Lmax;
-	//int *Lmin = *Lmin;
-	//int sz = m * n;
 
 	/* Core function */
 
-	// Find maximas
-	//int *Pmax[sz0] = {0};
-	//double *Vmax[sz0] = {0};
 	Pmax[0] = 0;
 	Vmax[0] = Y[0];
 	*Lmax = 1;
@@ -46,17 +32,7 @@ void find_extrema(int *Pmax, double *Vmax, int *Pmin, double *Vmin, int *Lmax, i
 		if (test > Vmax[*Lmax-1])
 			Vmax[*Lmax-1] = test;
 	}
-	//fout << "pmax = [";
-	//for (int i=0; i < *Lmax; i++)
-	//	fout << Pmax[i] << " ";
-	//fout << "]" << endl;
-	//fout << "vmax = [";
-	//for (int i=0; i < *Lmax; i++)
-	//	fout << Vmax[i] << " ";
-	//fout << "]" << endl;
-	// Find minimas
-	//int *Pmin[sz0] = {0};
-	//double *Vmin[sz0] = {0};
+
 	Pmin[0] = 0;
 	Vmin[0] = Y[0];
 	*Lmin = 1;
@@ -80,13 +56,6 @@ void find_extrema(int *Pmax, double *Vmax, int *Pmin, double *Vmin, int *Lmax, i
 		if (test < Vmin[*Lmin-1])
 			Vmin[*Lmin-1] = test;
 	}
-	//fout << "pmin = [";
-	//for (int i=0; i < *Lmin; i++)
-	//	fout << Pmin[i] << " ";
-	//fout << "]" << endl;
-	//fout << "vmin = [";
-	//for (int i=0; i < *Lmin; i++)
-	//	fout << Vmin[i] << " ";
-	//fout << "]" << endl;
+
 
 }
