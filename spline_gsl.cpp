e1407ce1bd3ce554b067fd2c776c8ea32515ce5a
Po-Nan Li <lishantreasure@gmail.com>
2014-11-16 23:59:56 +0800

    use polynomial for m1 <= 3

--- spline_gsl.cpp
+++ spline_gsl.cpp
@@ -10,9 +10,9 @@ void spline(double *YY,
 	// m2: length of original data points
 
 
-	gsl_interp_accel *acc = gsl_interp_accel_alloc ();
+	
   	//const gsl_interp_type *t = gsl_interp_cspline; 
-  	gsl_spline *spline = gsl_spline_alloc (gsl_interp_cspline, m1);
+  	
 
 	/* Core function */
   	double *xd = new double[m1];
@@ -23,21 +23,38 @@ void spline(double *YY,
 
 	double m;
 	
-	if (m1 > 3 ) {
+	if (m1 > 3 ) { // use spline
+		gsl_interp_accel *acc = gsl_interp_accel_alloc ();
+		gsl_spline *spline = gsl_spline_alloc (gsl_interp_cspline, m1);
+		gsl_spline_init (spline, xd, Y, m1);
 		for (int j = 0; j < m2; j++) {
 			YY[j] = gsl_spline_eval (spline, j, acc);
 		} // end of for-j
 	} // end of if
 
 	else {
-		m = (Y[1] - Y[0]) / (m2 - 1);
-		for (int j = 0; j < m2; j++) {
-			YY[j] = Y[0] + m * j;		
+		if (m1 > 2) { // use polynomial
+			gsl_interp_accel *acc = gsl_interp_accel_alloc ();
+			gsl_spline *spline = gsl_spline_alloc (gsl_interp_polynomial, m1);
+			gsl_spline_init (spline, xd, Y, m1);
+			for (int j = 0; j < m2; j++) {
+			YY[j] = gsl_spline_eval (spline, j, acc);
+		} // end of for-j
+
+		} // end of if
+		else {
+			m = (Y[1] - Y[0]) / (m2 - 1);
+			for (int j = 0; j < m2; j++) {
+				YY[j] = Y[0] + m * j;
+		} // end of else	
 		} // end of for-j	
 	} //end of else
 
 	delete[] xd;
-	gsl_interp_accel_free (acc);
-	gsl_spline_free (spline);
+	if (m1 > 2) {
+		gsl_interp_accel_free (acc);
+		gsl_spline_free (spline);	
+	}
+	
 
 }
